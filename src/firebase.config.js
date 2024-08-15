ce188ea8185346a2cf61d2c46959af67d348c94a
hrmrakib <hrmrakibs@gmail.com>
2024-08-15 09:32:02 +0600

    initial set-up

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA--2rQ6kCl7qKMu59NXpobqfoGhanwQkc",
+  authDomain: "scic-task1.firebaseapp.com",
+  projectId: "scic-task1",
+  storageBucket: "scic-task1.appspot.com",
+  messagingSenderId: "372887411364",
+  appId: "1:372887411364:web:56b13cc0e63bf11b63e431",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
